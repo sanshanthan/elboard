71b1b4e99eb445bdadec81fab86613d58a68d635
sanshanthan <sanshanthan@yahoo.com>
2021-10-05 00:28:25 +0530

    Splash screen Init

---
+++ lib/splash.dart
@@ -0,0 +1,57 @@
+import 'dart:async';
+
+import 'package:elboard/home.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class Splash extends StatefulWidget {
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<Splash> {
+  startSplashScreenTimer() async {
+    var _duration = new Duration(seconds: 5);
+    return new Timer(_duration, navigateToPage);
+  }
+
+  void navigateToPage() {
+    Navigator.pushReplacement(
+      (context),
+      MaterialPageRoute(
+          builder: (context) => MyHomePage(
+                title: 'ELBOARD',
+              )),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    startSplashScreenTimer();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([]);
+
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.fitHeight,
+          image: ExactAssetImage("assets/Logo.PNG"),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setEnabledSystemUIOverlays(SystemUiOverlay.values);
+    super.dispose();
+  }
+}
