5db45b14e88f38785c70dcab4bbf1ac9ca397a94
sanshanthan <sanshanthan@yahoo.com>
2021-11-03 01:20:32 +0530

    screens

--- lib/home.dart
+++ lib/home.dart
@@ -1,44 +1,59 @@
+import 'package:elboard/screens/market_screen.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
-
   final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int currentTabIndex = 0;
+  List<Widget> tabs = [
+    MarketScreen(Colors.green),
+    MarketScreen(Colors.blue),
+    MarketScreen(Colors.orange),
+    MarketScreen(Colors.amber),
+    MarketScreen(Colors.cyan)
+  ];
+  onTapped(int index) {
+    setState(() {
+      currentTabIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text("Select your community"),
       ),
-      body: Center(),
+      body: tabs[currentTabIndex],
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
+        onTap: onTapped,
+        currentIndex: currentTabIndex,
         items: [
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: "Home",
+            icon: Icon(Icons.shop_two),
+            label: "Market",
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.mail),
-            label: "Messages",
+            icon: Icon(Icons.person_add_alt_1_rounded),
+            label: "Neighbor",
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.person),
-            label: "Profile",
+            label: "Requester",
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.access_alarms),
-            label: "Profile2",
+            icon: Icon(Icons.menu_book),
+            label: "Directory",
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.access_time_rounded),
-            label: "Profile3",
+            icon: Icon(Icons.menu),
+            label: "menu",
           )
         ],
       ),
---
+++ lib/screens/market_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MarketScreen extends StatelessWidget {
+  final Color color;
+  MarketScreen(this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      color: color,
+    );
+  }
+}
